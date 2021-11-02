a98a3687db77c7e1c8c55bea2c9faa0039c668b1
klebaniukp <klebaniukp@gmail.com>
2021-11-02 22:54:38 +0100

    bootstrap connected | new routing dependencies REACT

--- lerna-repo/packages/client/src/App.tsx
+++ lerna-repo/packages/client/src/App.tsx
@@ -2,7 +2,32 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return <div className="App">djdjjdj</div>;
+    return (
+        <div>
+            <ul className="nav">
+                <li className="nav-item">
+                    <a className="nav-link active" href="#">
+                        Active
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        Link
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">
+                        Link
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link disabled" href="#">
+                        Disabled
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
